aea0424dbf031ab5bfe1e675bf2e79c5387f3ae9
Yeoh Sing Hsia <yeohmj15@gmail.com>
2024-12-28 15:10:52 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -25,14 +25,24 @@ auth_uri = st.secrets["google"]["auth_uri"]
 token_uri = st.secrets["google"]["token_uri"]
 redirect_uris = st.secrets["google"]["redirect_uris"]
 
-def authenticate_drive_api():
-    # Get credentials from Streamlit secrets
-    client_id = st.secrets["google"]["client_id"]
-    client_secret = st.secrets["google"]["client_secret"]
 
-    # Prepare OAuth flow using the credentials from Streamlit secrets
+def authenticate_drive_api():
+    # Get the installed credentials section from Streamlit secrets
+    client_id = st.secrets["google.installed"]["client_id"]
+    client_secret = st.secrets["google.installed"]["client_secret"]
+
+    # Create the configuration dictionary for the OAuth flow
+    client_config = {
+        "installed": {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "redirect_uris": ["http://localhost"]
+        }
+    }
+
+    # Initialize OAuth flow
     flow = InstalledAppFlow.from_client_config(
-        {"installed": {"client_id": client_id, "client_secret": client_secret, "redirect_uris": ["http://localhost"]}},
+        client_config,
         scopes=["https://www.googleapis.com/auth/drive.readonly"]
     )
 
@@ -55,6 +65,7 @@ def authenticate_drive_api():
 
     # Build the API client
     return build("drive", "v3", credentials=creds)
+
 
 # Call the authenticate function to initialize the API client
 drive_service = authenticate_drive_api()
